f150e8331b2432367d7012fd8b57671c84c466f6
trewer68 <45714912+trewer68@users.noreply.github.com>
2023-07-27 00:27:08 +0300

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Xуйня
+// @namespace    https://github.com/trewer68/twitter_xyinia
+// @version      0.1
+// @description  .
+// @author       trewer68
+// @match        https://twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @updateURL    https://github.com/trewer68/twitter_xyinia/script.user.js
+// @downloadURL  https://github.com/trewer68/twitter_xyinia/script.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+    setTimeout(function() {
+        document.getElementsByClassName("css-901oao css-16my406 css-1hf3ou5 r-poiln3 r-1inkyih r-rjixqe r-bcqeeo r-qvutc0")[0].innerHTML = "уйня"
+    }, 800);
+
+    // Your code here...
+})();
